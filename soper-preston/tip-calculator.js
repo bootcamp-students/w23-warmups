8f06aa9c53e3c1be11e625f00f7e64452260f63d
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-06 13:38:15 +0000

    I can not spell

--- soper-preston/tip-calculator.js
+++ soper-preston/tip-calculator.js
@@ -4,4 +4,4 @@
 //Using indexOf to get the correct spot in the array and toLowerCase to make it lowercase to keep the same 
 
 calculateTip = (amount, rating) => (tipScale => tipScale < 0 ? 'Rating not recognised' : Math.ceil(amount * tipScale * .05))
-(['terrible', 'poor', 'good', 'great', 'excellent'].indexOf(rating.toLowerCase()))
+(['terrible', 'poor', 'good', 'great', 'excellent'].indexOf(rating.toLowerCase())) 
