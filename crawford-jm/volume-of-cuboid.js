781afc7688aca5f0be1554beef0c7ad08eee8de6
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-02-07 08:56:50 -0500

    Create volume-of-cuboid.js

---
+++ crawford-jm/volume-of-cuboid.js
@@ -0,0 +1,8 @@
+//Class makes object kata
+class Kata {
+    //Static declares method l, w, h
+    static getVolumeOfCuboid(length, width, height) {
+      //Return calculation of l * w * h
+      return length * width * height
+    }
+  }
