9d9d385c6118b68549318dcc568677a7ebc361fd
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-02-07 13:37:38 +0000

    added solution

---
+++ tryon-baldwin/div-by-d2.js
@@ -0,0 +1,13 @@
+function isDivisible(n, x, y) {
+
+  
+    //  START
+   //  READ n, x, y
+    // IF (n is divisble by x AND n is divisible by y)
+   //    SHOW true
+  //   ELSE
+   //    SHOW false
+   //  ENDIF
+    // END
+  return n % x === 0 && n % y === 0
+ }
