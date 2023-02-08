a4e80cea2dbb8aee9863259b1e5f8c4364989aa8
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-02-08 13:35:26 +0000

    added katas for 2-8-23

---
+++ thomas-zachary/will-you-make=it.js
@@ -0,0 +1,6 @@
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    // return boolean
+    // multiply mpg by fuelleft
+    // compare result to distance
+    return distanceToPump <= mpg * fuelLeft
+  };
