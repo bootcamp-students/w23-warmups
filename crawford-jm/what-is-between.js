7fc83483b1b8e63a4ceb2c4b88e7562b559be307
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-02-08 08:44:44 -0500

    Warm ups Day three kata 1

---
+++ crawford-jm/what-is-between.js
@@ -0,0 +1,12 @@
+//Function named between takes integers a and b
+function between(a, b) {
+  //Declare variable named arr with empty array
+  let arr = [];
+  //For loop iterating through arr
+  for (let i = a; i <= b; i++) {
+    //Push results of for loop to arr
+    arr.push(i);
+  }
+  //Return variable arr
+  return arr;
+}
