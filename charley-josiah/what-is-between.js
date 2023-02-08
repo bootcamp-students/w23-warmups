2849a33dec68bd04d0afe6dc83a8b2b09c4733c9
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-02-08 14:39:47 +0000

    added files

---
+++ charley-josiah/what-is-between.js
@@ -0,0 +1,10 @@
+function between(a, b) {   
+    // your code here   
+    let numbers = [];      
+    
+    for(let i = a; i <= b; i++) {
+        numbers.push(i);    
+     }   
+    
+    return numbers 
+}
