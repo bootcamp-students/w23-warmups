c4d3f22ebe25bd3561a7e80a71cad74a66634c75
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-02-09 08:58:43 -0500

    Update century-from-year.js

--- crawford-jm/century-from-year.js
+++ crawford-jm/century-from-year.js
@@ -1,5 +1,5 @@
-function noSpace(x){
-//Split string into array of substrings without spaces
-  //Join string backtogether
-  return x.split(" ").join('');
+//Given the year, find the century
+function century(year) {
+  //Round up result of year divided by 100 to whole number 
+  return Math.ceil(year/100);
 }
