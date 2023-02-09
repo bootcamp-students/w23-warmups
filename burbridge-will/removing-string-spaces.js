62081e2c39ba3317057a4c2e534d19dceebcb8be
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-02-09 16:34:37 +0000

    Warmups Day 4: Removing String Spaces, Century from Year

---
+++ burbridge-will/removing-string-spaces.js
@@ -0,0 +1,5 @@
+function noSpace(x){
+    return x.split(" ").join('');
+    /*This splits the string into an array of individual strings 
+    and then joins them together without the spaces*/
+  }
