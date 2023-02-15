f78f588e6859d129d23f10596bc91b23147f0c65
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-15 13:03:02 +0000

    warmups day 8

---
+++ soper-preston/wholikesit.js
@@ -0,0 +1,14 @@
+function likes(names) {
+    var templates = [
+      'no one likes this',
+      '{name} likes this',
+      '{name} and {name} like this',
+      '{name}, {name} and {name} like this',
+      '{name}, {name} and {n} others like this'
+    ];
+    var idx = Math.min(names.length, 4);
+    
+    return templates[idx].replace(/{name}|{n}/g, function (val) {
+      return val === '{name}' ? names.shift() : names.length;
+    });
+  }
