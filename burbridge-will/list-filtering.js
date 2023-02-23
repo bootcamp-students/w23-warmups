498a8fa99b127e7b60d763cad0502ae56a38136a
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-02-23 14:33:50 +0000

    Warmups Day 14: List Filtering

---
+++ burbridge-will/list-filtering.js
@@ -0,0 +1,13 @@
+function filter_list(l) {
+    console.log(l);
+    let fl = l.filter(item => typeof item != "string");
+    console.log(fl);
+    return fl;
+  }
+  /*
+    Pseudocode
+    START
+    read array l
+    Filter out elements that have a type of 'string', store it in new array fl
+    RETURN fl
+  */
