01ef5f82ebfe7db695c348d265b4bbcd6d2ca532
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-08 09:03:33 -0500

    Create useMap-to-double.js

---
+++ crawford-jm/useMap-to-double.js
@@ -0,0 +1,10 @@
+function double(array) {
+  //console.log(array);
+    let newArr = array.map(num => num * 2);
+    //console.log(newArr);
+  return newArr;
+}
+
+
+//Function takes an array of integers
+//Return an array where each integer is doubled in value
