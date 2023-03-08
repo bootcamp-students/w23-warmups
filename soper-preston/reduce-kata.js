2240f99d49db936236e8026b4c42cf2de48807a8
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-08 13:34:25 +0000

    warmups 24?

---
+++ soper-preston/reduce-kata.js
@@ -0,0 +1,11 @@
+function sum(array) {
+    // Use array.reduce() to calculate and return the
+    // sum of the values in array.
+    const initValue = 0;
+  const sum = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initValue
+  );
+    return sum;
+  }
+  
