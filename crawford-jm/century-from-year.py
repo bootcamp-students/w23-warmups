d65ae19b68187951e9ddf93799255b53057eaac1
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-20 08:23:05 -0400

    Create century-from-year.py

---
+++ crawford-jm/century-from-year.py
@@ -0,0 +1,12 @@
+def century(year):
+    import math
+    print(math.ceil(year/100))
+    return (math.ceil(year/100))
+
+
+#Function takes a year
+#Return the century
+
+#import math function
+#divide year by 100
+#use math.ceil to round number up
