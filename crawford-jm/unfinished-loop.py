5d5305090e94a092d7ac468d05bd23a27c664dc8
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-20 08:24:53 -0400

    Create unfinished-loop.py

---
+++ crawford-jm/unfinished-loop.py
@@ -0,0 +1,14 @@
+def create_array(n):
+    res=[]
+    i = 1
+    while i <= n: 
+        res.append(i)
+        print(i)
+        i += 1
+    return res
+
+
+#print(create_array(10))
+
+#function is currently an infinite loops
+#debug function
