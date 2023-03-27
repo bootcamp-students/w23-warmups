98598319ba507faf924f4c3ad0b93897d5fb241f
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-27 10:48:03 -0400

    Create concatenating-columns.sql

---
+++ crawford-jm/concatenating-columns.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(prefix, ' ', first, ' ', last, ' ', suffix) as title
+FROM names;
+
+
+--Use a select statement
+--Return a single column table...
+  --Containing the full title of the person
+  --concatenate all columns together except id
